af5e283bfafe94fd51a49bd960a09dbd20cf987b
Francesco Paolocci <micky5991@gmail.com>
2022-07-08 19:27:23 +0200

    Rename page to src

--- src/server.js
+++ src/server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, './public/')));
 
 app.get("/", (req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    fs.createReadStream("index.html").pipe(res);
+    fs.createReadStream(path.resolve(__dirname, './index.html')).pipe(res);
 });
 
 app.get("/current", (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    fs.createReadStream("./currentvideo.json").pipe(res);
+    fs.createReadStream(path.resolve(__dirname, './currentvideo.json')).pipe(res);
     res.writeHead(200);
 });
 
@@ -26,7 +27,7 @@ app.post("/", (req, res) => {
     console.log(`${Date.now()} - POST received`);
 
     req.on("end", () => {
-        return fs.writeFileSync("./currentvideo.json", body);
+        return fs.writeFileSync(path.resolve(__dirname, './currentvideo.json'), body);
     });
     console.log(`${Date.now()} - POST saved`);
 });
